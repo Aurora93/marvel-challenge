6c7265f1a0cd7ed8917288e0f058432113db3591
Aurora93 <aurora.perez1993@gmail.com>
2024-05-02 18:28:27 +0200

    update searchQueryParamsFormater test

--- src/utils/searchQueryParamsFormater.test.ts
+++ src/utils/searchQueryParamsFormater.test.ts
@@ -5,33 +5,36 @@ describe("searchQueryParamsFormater", () => {
   it("should return correctly when a search term is passed", () => {
     // Arrange
     const term = "Iron Man";
+    const filters = { term };
 
     // Act
-    const result = searchQueryParamsFormater(term);
+    const result = searchQueryParamsFormater(filters);
 
     // Assert
-    expect(result).toEqual({ name: term });
+    expect(result).toEqual({ name: term, limit: 6 });
   });
 
   it("should return correctly when a single character search term is passed", () => {
     // Arrange
     const term = "A";
+    const filters = { term };
 
     // Act
-    const result = searchQueryParamsFormater(term);
+    const result = searchQueryParamsFormater(filters);
 
     // Assert
-    expect(result).toEqual({ nameStartsWith: term });
+    expect(result).toEqual({ nameStartsWith: term, limit: 6 });
   });
 
   it("should returns an empty object when no search term is passed", () => {
     // Arrange
     const term = undefined;
+    const filters = { term };
 
     // Act
-    const result = searchQueryParamsFormater(term);
+    const result = searchQueryParamsFormater(filters);
 
     // Assert
-    expect(result).toEqual({});
+    expect(result).toEqual({ limit: 6 });
   });
 });
